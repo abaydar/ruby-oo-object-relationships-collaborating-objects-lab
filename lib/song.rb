20c2dfff06c05c5a9521fa9b40c21adfabf86f29
+ <+>
2020-12-04 09:40:03 -0500

    commit tree be73c44ef6779ef85d4c28627ac35dc3155c9105

--- lib/song.rb
+++ lib/song.rb
@@ -16,20 +16,20 @@ class Song
         @@all
     end
 
-    def self.new_by_filename(filename)
-        artist = filename.split(" - ")[0] #parsing to split input into artist & song
-        song = filename.split(" - ")[1]
-        new_song = self.new(song) #create a new song using song string from filename
-        new_song.artist_name = artist #use new_song.artist_name so the following method can convert string into an Artist instance?
-        new_song
-    end
-
     # def self.new_by_filename(filename)
-    #     song = self.new
-    #     song.name = filename.split(" - ")[1]
-    #     song.artist = filename.split(" - ")[0]
-    #     song
+    #     artist = filename.split(" - ")[0] #parsing to split input into artist & song
+    #     song = filename.split(" - ")[1]
+    #     new_song = self.new(song) #create a new song using song string from filename
+    #     new_song.artist_name = artist #use new_song.artist_name so the following method can convert string into an Artist instance?
+    #     new_song
     # end
+
+    def self.new_by_filename(filename)
+        song = self.new(filename.split(" - ")[1])
+        artist_name=Artist.find_or_create_by_name(filename.split(" - ")[0])
+        song.artist = artist_name
+        song
+    end
 
     def artist_name=(name) #writer/setter
         self.artist = Artist.find_or_create_by_name(name) #change the artist "string" from filename into an Artist instance via the method find_or_create_by_name
